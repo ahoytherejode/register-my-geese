ad175a3d83267fd1de5a2534f3e3593cac24270c
“Jodie <jodie-leigh.page@landregistry.gov.uk>
2020-11-30 16:05:44 +0000

    Initialising Git Repository

---
+++ app/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+/* router.post('/04-summary'), function (req, res) {
+  if (req.session.data[''] = );
+  } else {
+    res.redirect('page');
+  }
+}*/
+
+// Add your routes here - above the module.exports line
+module.exports = router
